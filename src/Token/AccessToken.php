eea3a2f3d09d0ba20c65aa712d772487477e7c2c
CharleaEasy <489644865@qq.com>
2022-07-21 00:32:10 +0800

    update TokenApi

--- src/Token/AccessToken.php
+++ src/Token/AccessToken.php
@@ -1,12 +1,49 @@
 <?php
 
+namespace CharEasy\LarkApi;
+
+use GuzzleHttp\Client;
+
 /**
  * Don't let love live in memory.
- * user: WangHui
+ * user: CharEasy
  * Date：2022/7/20
  * Time: 21:17
  */
 class AccessToken
 {
+    private $_appId;
+    private $_appSecret;
+
+    private $_apiUrl = 'https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal';
+
+//    private $_apiUrl = 'https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal';
+
+    public function __construct($appid, $appSecret)
+    {
+        $this->_appId = $appid;
+        $this->_appSecret = $appSecret;
+    }
+
+    public function getAccessToken()
+    {
+        $client = new Client();
+        $header = [
+            'content-type' => 'application/json; charset=utf-8'
+        ];
+        $params = [];
+        $params['app_id'] = $this->_appId;
+        $params['app_secret'] = $this->_appSecret;
+        $response = $client->request('POST', $this->_apiUrl, [
+            'headers' => $header,
+            'body' => json_encode($params)
+        ]);
+        $httpCode = $response->getStatusCode();
+        if ($httpCode == 200) {
+            return json_decode($response->getBody()->getContents(), 1);
+        } else {
+            return [];
+        }
+    }
 
 }
